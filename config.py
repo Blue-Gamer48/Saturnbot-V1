dbf16029a8b79807f5073047778b2c2253fb5a5d
Blue-Gamer48 <97996264+Blue-Gamer48@users.noreply.github.com>
2022-12-22 22:05:16 +0100

    Edit Config. py
    Remove Bottolen

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import discord
-BOT_NAME = "Alman Bot"
+BOT_NAME = "Saturnbot"
 STATUS = discord.Status.online
 ACTIVITY_LIST = [
     "ein Bot von Bluer_Gamer48",
@@ -9,4 +9,4 @@ ACTIVITY_LIST = [
 ]
 INVITE_LINK = "https://discord.com/oauth2/authorize?client_id=923162114187747328&permissions=8&scope=bot"
 WEBSITE_LINK = "https://saturn-bot.webnode.com/"
-DISCORD_TOKEN = "OTM1MjM5MDQ5MjU2NTIxODY4.Ye7vPQ.UTKkGPfCHB8qXv19XPly4hj3TQM"
+DISCORD_TOKEN = "TOKEN"
